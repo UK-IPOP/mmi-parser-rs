6984c4b62bf5b552c11349faff12840798b25e16
Nick Anthony <nanthony007@gmail.com>
2022-04-04 14:05:38 -0400

    Add sample data files.

--- src/main.rs
+++ src/main.rs
@@ -11,33 +11,29 @@ use serde_json::{self, Map, Value};
 fn main() {
     println!("{}", "MMI Parser".bright_green().bold());
     println!("{}", "============".bright_green().bold());
-    // let args: Vec<String> = env::args().collect();
-    // let folder = &args[1];
-    // println!("Reading files from: {}", folder);
-    // match fs::read_dir(folder) {
-    //     Ok(files) => {
-    //         for file in files {
-    //             let file = file.unwrap();
-    //             let path = file.path();
-    //             let filename = path.to_str().unwrap();
-    //             println!("Reading file: {:}", filename);
-    //             let contents = fs::read_to_string(filename).unwrap();
-    //             let mut data: serde_json::Value =
-    //                 serde_json::from_str(&contents).expect("could not parse json");
-    //             parse_mmi_from_json(&mut data);
-    //             let json_string = serde_json::to_string(data).expect("could not serialize json");
-    //             println!("Writing file: {:}", filename);
-    //             fs::write(filename, json_string)?;
-    //         }
-    //     }
-    //     Err(e) => println!("{}", "Error: ".bright_red().bold() + e.to_string()),
-    // }
-
-    let file2 = "data/sample.json";
-    let contents2 = fs::read_to_string(file2).expect("Something went wrong reading file2");
-    let mmi_outputs: serde_json::Value = serde_json::from_str(&contents2).unwrap();
-    let added_model = mmi_parser::parse_mmi_from_json(mmi_outputs);
-    let json_string = serde_json::to_string(&added_model).unwrap();
-    let mut f = File::create("data/sample_output.json").unwrap();
-    f.write_all(json_string.as_bytes()).unwrap();
+    let args: Vec<String> = env::args().collect();
+    let folder = &args[1];
+    println!("Reading files from: {}", folder);
+    match fs::read_dir(folder) {
+        Ok(files) => {
+            for file in files {
+                let file = file.unwrap();
+                let path = file.path();
+                let filename = path.to_str().unwrap();
+                if filename.ends_with(".json") {
+                    println!("Reading file: {:}", filename);
+                    let contents =
+                        fs::read_to_string(file.path()).expect("Something went wrong reading file");
+                    let json_data: serde_json::Value = serde_json::from_str(&contents).unwrap();
+                    let modeled = mmi_parser::parse_mmi_from_json(json_data);
+                    let json_string = serde_json::to_string(&modeled).unwrap();
+                    let outname = filename.replace(".json", "_parsed.json").to_string();
+                    println!("Writing file: {:}", outname.as_str());
+                    fs::write(outname.as_str(), json_string.as_bytes())
+                        .expect("Unable to write file");
+                }
+            }
+        }
+        Err(e) => println!("Error: {}", e.to_string()),
+    }
 }
