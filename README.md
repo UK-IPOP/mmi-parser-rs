452fd59538f649be6851fb1e89bfd31d13d448b5
Nick Anthony <nanthony007@gmail.com>
2022-04-11 15:20:41 -0400

    Update README. Add txt file support in main.rs

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ We use [this](https://lhncbc.nlm.nih.gov/ii/tools/MetaMap/Docs/MMI_Output_2016.p
 This expects MetaMap 2016 version due to output changes.
 
 This project currently does **no validation** with the expectation that MetaMap is correct (it almost always is) and that no manipulation has been done to the data.
+
+TODO:
+- [ ] 
+- [ ] Update tests
+- [ ] Test coverage (missing MMI)
+- [ ] Determine private/public API barrier
+  - This is in a good place by just exposing MmiOutput?
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{
     env,
     fs::{self},
 };
+use std::fs::remove_file;
 
 use colored::*;
 
@@ -12,6 +13,8 @@ fn main() {
     println!("{}", "============".bright_green().bold());
     let args: Vec<String> = env::args().collect();
     let folder = &args[1];
+    remove_file("data/sample_parsed.json").unwrap();
+    remove_file("../data/sample_parsed.txt.json").unwrap();
     println!("Reading files from: {}", folder);
     match fs::read_dir(folder) {
         Ok(files) => {
@@ -27,6 +30,28 @@ fn main() {
                     let modeled = mmi_parser::parse_mmi_from_json(json_data);
                     let json_string = serde_json::to_string(&modeled).unwrap();
                     let outname = filename.replace(".json", "_parsed.json").to_string();
+                    println!("Writing file: {:}", outname.as_str());
+                    fs::write(outname.as_str(), json_string.as_bytes())
+                        .expect("Unable to write file");
+                }
+            }
+        }
+        Err(e) => println!("Error: {}", e.to_string()),
+    }
+    match fs::read_dir(folder) {
+        Ok(files) => {
+            for file in files {
+                let file = file.unwrap();
+                let path = file.path();
+                let filename = path.to_str().unwrap();
+                if filename.ends_with(".txt") {
+                    println!("Reading file: {:}", filename);
+                    let contents =
+                        fs::read_to_string(file.path()).expect("Something went wrong reading file");
+                    let result = mmi_parser::parse_mmi(&contents);
+                    let json_val = serde_json::to_value(result).expect("unable to create json");
+                    let json_string = serde_json::to_string(&json_val).unwrap();
+                    let outname = filename.replace(".txt", "_parsed_txt.json").to_string();
                     println!("Writing file: {:}", outname.as_str());
                     fs::write(outname.as_str(), json_string.as_bytes())
                         .expect("Unable to write file");
